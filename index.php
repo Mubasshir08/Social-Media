02125987729a5b0113db096ac8c1c3759253bc90
Mubasshir08 <msr4212019@gmail.com>
2022-06-23 03:46:15 -0700

    login Page & SignUp Page Created

--- index.php
+++ index.php
@@ -5,21 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Social-Media</title>
-    <!-- Goolge_Font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <!------------------------->
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-   <header>
-    <ul>
-        <li><a href="home.php">Home</a></li>
-        <li><a href="profile.php">Profile</a></li>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="signup.php">Signup</a></li>
-    </ul>
-   </header>
+    <?php require_once "header.php"; ?>
+    <?php require_once "footer.php"; ?>
+    
 </body>
 </html>
